3bc83e2027d1fb66f9fe4b225576419c3dbc4350
Kane Cleveland <enaklmao@gmail.com>
2025-02-20 19:53:36 -0500

    random change gonna do work remotely prolly

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from api.script_processor import ScriptProcessor  # noqa: F401
 
 
 def main():
-    proc = ScriptProcessor(sift_file="C:/Users/Kane/projects/Sift/siftscripts/sample2_long.sift", show_ast=True)
+    proc = ScriptProcessor(sift_file="C:/Users/Kane/projects/Sift/siftscripts/sample2_long.sift", show_ast=True, save_json=True)
     proc.to_queue()
 if __name__ == "__main__":
     main()
